eb85ebb97f7dd0db07b63b0142c794b86cc40e9f
obsidiannnn <satya.chauhan1976@gmail.com>
2026-01-21 19:48:34 +0530

    updates in go

--- GoLang/Day_1/main.go
+++ GoLang/Day_1/main.go
@@ -1,6 +1,11 @@
+//gettings starting with go
+
 package main
 
 import "fmt"
+
+// main() is the entry point
+// run a go file -> go looks for package main and func main
 
 func main() {
 	fmt.Println("Hello Go")
