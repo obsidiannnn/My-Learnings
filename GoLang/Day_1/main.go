6fef83cac7d5bedff3065826b8a5794bed65572a
obsidiannnn <satya.chauhan1976@gmail.com>
2026-01-27 20:00:19 +0530

    reactjs

--- GoLang/Day_1/main.go
+++ GoLang/Day_1/main.go
@@ -11,8 +11,16 @@ import (
 
 func main() {
 	fmt.Println("Hello Go")
+
 	Math()
+
 	// We can directly use other go files in main file if
 	// they are in same folder and are of same package
+
 	variables()
 }
+
+// Inside a go folder there will be one main.go file
+// that will run the code , we will code in other files
+// to but have to call those fnc inside the main files's
+// func main to run them.
