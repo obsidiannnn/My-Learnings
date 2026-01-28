15ccf2c21713c5102ece08de57ad5a88747396b0
obsidiannnn <satya.chauhan1976@gmail.com>
2026-01-28 14:09:39 +0530

    Public var in go

--- GoLang/Day_2/shortvardeclaration.go
+++ GoLang/Day_2/shortvardeclaration.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+// if u want to create a global/public variable in go
+// Variable name must start with capital letter
+// and no inferred var declaration is allowed .
+
+var Public bool = true
+
 func shortdeclaration() {
 	var city string
 	city = "London"
@@ -18,5 +24,6 @@ func shortdeclaration() {
 	currentmoney += 1000
 
 	fmt.Println(city, name, currentmoney)
+	fmt.Println(Public)
 
 }
