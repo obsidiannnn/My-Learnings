51460b1683ea5ce202006c588777a922ab96744c
obsidiannnn <satya.chauhan1976@gmail.com>
2026-01-29 18:56:28 +0530

    First React

--- ReactJS/ReactJs/src/main.jsx
+++ ReactJS/ReactJs/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import Header from "./components/Header.jsx";
+import MainContent from "./components/Maincontent.jsx";
+import Footer from "./components/Footer.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <Header name="My Website" />
+    <MainContent />
+    <Footer email="my@email.com"/>
+  </>,
+);
