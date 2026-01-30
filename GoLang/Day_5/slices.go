26a1f1686d755a5f558517bb21d5061ac3c56878
obsidiannnn <satya.chauhan1976@gmail.com>
2026-01-30 14:49:45 +0530

    slices

--- GoLang/Day_5/slices.go
+++ GoLang/Day_5/slices.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"fmt"
+)
+
+func Slice() {
+
+	var items = []string{"One", "Two", "Three"}
+
+	// while defining if we give fixed number of elements it's a array
+	// if we don't give then it's a slice
+
+	fmt.Println(items)
+	fmt.Printf("Type of itmes is %T \n", items)
+}
