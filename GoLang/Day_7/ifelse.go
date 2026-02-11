431b8ee51665c861e3e437951c779448301dc824
obsidiannnn <satya.chauhan1976@gmail.com>
2026-02-11 22:46:55 +0530

    initializing and checking at the same time

--- GoLang/Day_7/ifelse.go
+++ GoLang/Day_7/ifelse.go
@@ -20,5 +20,17 @@ func ifelse(){
 	}
 	fmt.Println(result)
 
+	if 9%2 == 0 {
+		fmt.Println("Number is even")
+	} else {
+		fmt.Println("Not even")
+	}
 
+	// initializing and checking for the condition at the same time
+
+	if num := 3; num <10 {
+		fmt.Println("Number is less than 10")
+	} else {
+		fmt.Println("Greater than 10")
+	}
 }
