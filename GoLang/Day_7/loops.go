6f67d3759b37b2b83315f2742bb5afb513370bfc
obsidiannnn <satya.chauhan1976@gmail.com>
2026-02-12 11:40:03 +0530

    for loops with range

--- GoLang/Day_7/loops.go
+++ GoLang/Day_7/loops.go
@@ -6,4 +6,20 @@ import (
 
 func loops(){
 	fmt.Println("loops in GoLang")
+
+	// we will be looping over slice as arrays are not dynamic in golang
+	days := []string{"Sunday","Tuesday","Wednesday","Friday","Saturday"}
+	fmt.Println(days)
+
+	// for loop:
+
+	for d:= 0; d<len(days); d++{
+		fmt.Println(days[d])
+	}
+
+	// for with range
+
+	for i := range days{
+		fmt.Println(days[i])
+	}
 }
