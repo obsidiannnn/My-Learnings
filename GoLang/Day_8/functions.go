d548e1274348e0c63765c1d14627808703163ab9
obsidiannnn <satya.chauhan1976@gmail.com>
2026-02-12 14:32:30 +0530

    Uncertain number of parameters

--- GoLang/Day_8/functions.go
+++ GoLang/Day_8/functions.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 )
 
-func main(){
+func main() {
 	fmt.Println("Functions in GO")
 
-	result := adder(24324,56336)
+	result := adder(24324, 56336)
 	fmt.Println(result)
-	
+
+	proresult := proadder(2, 3, 4, 5, 6,345,47,346,547,3254,6,8,546,46)
+	fmt.Printf("Pro result is %v \n", proresult)
+
 }
 
 // taking parameters in a func
@@ -19,4 +22,23 @@ func main(){
 
 func adder(valOne int, valTwo int) int {
 	return valOne + valTwo
+}
+
+// when we are not certain how many values will comes
+// then we use ... to take all values under common type
+
+//usign ... give us a slice of all values
+
+func proadder(values ...int) int {
+	total := 0
+
+	// for takes two values index and value at that index
+	// since we not dealing with index in this for
+	// that's why we used _
+
+	for _, val := range values {
+		total += val
+	}
+	return total
+
 }
