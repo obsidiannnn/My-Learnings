7bc5c0afe07974cb95ee64581b9e4f96b55fb929
obsidiannnn <satya.chauhan1976@gmail.com>
2026-02-17 23:17:29 +0530

    get request status code

--- GoLang/Day_10/GET.go
+++ GoLang/Day_10/GET.go
@@ -14,5 +14,17 @@ func main() {
 func getrequest() {
 	const newurl = "http://localhost:8000/get"
 
-	http.Get(newurl)
+	res, err := http.Get(newurl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	// once again api request never close on their own it is imp to
+	// close them at last.
+
+	fmt.Println("Status code: ", res.Status)
+
 }
