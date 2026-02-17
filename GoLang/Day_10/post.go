8b54bd5a4739943cf94934c0aac2a0b606f7356b
obsidiannnn <satya.chauhan1976@gmail.com>
2026-02-17 23:47:26 +0530

    requestbody

--- GoLang/Day_10/post.go
+++ GoLang/Day_10/post.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func post() {
 	fmt.Println("POST request in golang")
 
 	const urll = "http://localhost:8000/post"
-	
 
+	// fake json payload , payload is known as sending some json data
 
+	requestBody := strings.NewReader(`
+		{
+			"coursename" : "GoLang",
+			"price" : 0,
+			"platform" : "online"
+		}
+	`)
+
+	res, err = http.POST(urll, "application/json", requestBody)
 }
