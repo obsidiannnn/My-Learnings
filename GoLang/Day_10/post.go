22d51a15621e3af9e416c976c38e6e2f3a194be1
obsidiannnn <satya.chauhan1976@gmail.com>
2026-02-18 22:50:20 +0530

    io.ReadAll package

--- GoLang/Day_10/post.go
+++ GoLang/Day_10/post.go
@@ -24,12 +24,19 @@ func post() {
 
 	res, err := http.Post(urll, "application/json", requestBody)
 
+	// while using post request it goes in application/json format
+	// and we have to specify the content type as well and 
+	// then we have to pass the request in request body.
+
 	if err != nil {
 		panic(err)
 	}
 	defer res.Body.Close()
 
 	content, err := io.ReadAll(res.Body)
+
+	// io.ReadAll is used to read the response body 
+	// and it returns the content in byte format and error if any.
 
 	if err != nil {
 		panic(err)
