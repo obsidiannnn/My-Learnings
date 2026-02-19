a472c4e1d993ae8556c7db2c2785603fd460640e
obsidiannnn <satya.chauhan1976@gmail.com>
2026-02-20 01:13:26 +0530

    Structs tags

--- GoLang/Day_10/json.go
+++ GoLang/Day_10/json.go
@@ -1,33 +1,36 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 )
 
 // course starting with small c means we are not using it outside
 // this package and it is only for internal use and if we want to use it outside
 
+// structs are often used to represent data in jsson format
+// and we can use json tags to specify the json key for each field in the struct
 type course struct {
-	Name     string
-	Price    int
-	Platform string
-	Password string
-	Tags     []string
+	Name string `json:"Coursename"` // it will replace field name with coursename
+	// all over the data and it will be used as key in json data instead of Name
 
-	// tag will be a slice of string and it will 
-	// hold multiple tags for the course
+	Price    int
+	Platform string `json:"website"`
+	Password string `json:"-"` // after putting dash it will not be
+	// included in json data and it will be ignored while encoding
+	Tags []string `json: "tags,omitemty"` // after putting omitempty
+	// if the value of tags is empty then it will not be included in json data
+
+	// and it will return null as output.
 }
 
 func Encodingjson() {
-	// mycourse is a slice of course struct and it is empty for now 
+	// mycourse is a slice of course struct and it is empty for now
 	// and we will add some data to it later.
 	mycourse := []course{
 		{"ReactJS", 0, "online", "abc123", []string{"web-dev", "frontend"}},
 		{"GoLang", 0, "online", "def456", []string{"web-dev", "backend"}},
 		{"Python", 0, "online", "ghi789", nil},
-
-
 	}
 
 	// package this data as json data and send it to client
@@ -43,13 +46,12 @@ func Encodingjson() {
 
 	// here "" means we don't want any prefix and "\t" means we
 	// want to use tab for indentation and it will make data more readabl.
-	// /
 
 	if err != nil {
 		panic(err)
 	}
 
-	// json.Marshal is used to convert the data into json format 
+	// json.Marshal is used to convert the data into json format
 	// and it returns the json data in byte format and error if any.
 
 	fmt.Printf("final json is %s\n", string(finaljson))
