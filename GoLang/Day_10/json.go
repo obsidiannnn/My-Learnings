1b608d510fcb5eb8da393f0e9eab9b023d0be81f
obsidiannnn <satya.chauhan1976@gmail.com>
2026-02-19 20:32:17 +0530

    encoding data to json format

--- GoLang/Day_10/json.go
+++ GoLang/Day_10/json.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+// course starting with small c means we are not using it outside
+// this package and it is only for internal use and if we want to use it outside
+
+type course struct {
+	Name     string
+	Price    int
+	Platform string
+	Password string
+	Tags     []string
+}
+
 func Json() {
 	fmt.Println("Json data in go")
 }
