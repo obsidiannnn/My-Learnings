cea8329e33467c1cd39c44c3bd8b30991c4b9636
obsidiannnn <satya.chauhan1976@gmail.com>
2026-02-21 23:54:30 +0530

    json dataset

--- GoLang/Day_10/decodejson.go
+++ GoLang/Day_10/decodejson.go
@@ -4,7 +4,27 @@ import (
 	"fmt"
 )
 
-
 func DecodeJson() {
 	fmt.Println("Decodind json data")
+
+	// when data comes from net it is in byte format to amke it readble
+	// we use strings
+	// but sometimes we need data as it is in json format that's why we use
+	// []byte
+
+	jsondata := []byte(`
+		{
+			"coursename": "ReactJS Bootcamp",
+			"Price" : 299,
+			"Website": "HelloWorld.com",
+			"tags": ["web-dev","js"]
+		}
+		{
+			"coursename": "GoLang Bootcamp",
+			"Price" : 0,
+			"Website": "HelloWorld.com",
+			"tags": ["web-dev","go"]
+		}
+	
+	`)
 }
