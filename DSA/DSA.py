25743f1319e68fe9fa4b6b067948e22a4bc41901
obsidiannnn <satya.chauhan1976@gmail.com>
2026-03-08 23:59:13 +0530

    Fibonaci series via recursion

--- DSA/DSA.py
+++ DSA/DSA.py
@@ -55,3 +55,17 @@ def factorial(n):
         return 1
     return n * factorial(n-1)
 print(factorial(5))
+
+# Fibonacci Series using recursion
+
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)  
+print(fibonacci(10))
+
+
+
