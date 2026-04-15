dccf948903029b5e30644eafd09f664c38f4e39c
obsidiannnn <satya.chauhan1976@gmail.com>
2026-04-15 23:50:12 +0530

    demo data for prac project

---
+++ Backend_node/data.js
@@ -0,0 +1,28 @@
+export const jokes = [
+    {
+        id: 1,
+        title: 'A Joke',
+        content: 'This is a Joke'
+    },
+    {
+        id: 2,
+        title: 'Another Joke',
+        content: 'This is another Joke'
+    },
+    {
+        id: 3,
+        title: 'Third Joke',
+        content: 'This is third Joke'
+    },
+    {
+        id: 4,
+        title: 'Fourth Joke',
+        content: 'This is fourth Joke'
+    },
+    {
+        id: 5,
+        title: 'Fifth Joke',
+        content: 'This is fifth Joke'
+    }
+]
+
--- Backend_node/index.js
+++ Backend_node/index.js
@@ -1,12 +1,19 @@
-const express = require('express') // used to use express from dependencies
+// require('dotenv').config()
+//const express = require('express') // used to use express from dependencies
 
+import dotenv from 'dotenv'
+import express from 'express' // using express from dependencies with ES6 syntax
+import {jokes} from './data.js'
+dotenv.config()
 
 const app = express() // using express inside app
 
-const port = 3000
+
+const port = process.env.PORT || 5000
+
 
 app.get('/',(req,res)=>{
-    res.send("Hello")
+    res.send("Server is ready")
 })
 // '/' means it is listening on home route
 
@@ -15,8 +22,12 @@ app.get('/insta', (req,res)=>{
     // now server is listening on insta route
 })
 
+app.get('/jokes',(req,res)=>{
+    res.json(jokes)
+})
 
-app.listen(port, ()=>{
-    console.log(`App listening on port ${port}`);
+
+app.listen(port, ()=>{ // using port via env file
+    console.log(`Server listening on port ${port}`);
 })
 // server will listen on the route we hit
