3e4a0020f2081d42b9dd4d17c1d9399766779c36
Aditya chauhan <satya.chauhan1976@gmail.com>
2026-08-07 11:03:16 +0530

    starting rust

---
+++ Rust/target/debug/Rust.d
@@ -0,0 +1,1 @@
+E:\Learnings\My-Learnings\Rust\target\debug\Rust.exe: E:\Learnings\My-Learnings\Rust\src\main.rs
---
+++ Rust/target/debug/deps/Rust.d
@@ -0,0 +1,5 @@
+E:\Learnings\My-Learnings\Rust\target\debug\deps\Rust.d: src\main.rs
+
+E:\Learnings\My-Learnings\Rust\target\debug\deps\Rust.exe: src\main.rs
+
+src\main.rs:
