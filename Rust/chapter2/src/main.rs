0026623b4816c91b738d6da9d7c28f3c762dc1ce
Aditya chauhan <satya.chauhan1976@gmail.com>
2026-08-09 22:57:44 +0530

    reference with mut

--- Rust/chapter2/src/main.rs
+++ Rust/chapter2/src/main.rs
@@ -2,14 +2,15 @@ use std::io;
 
 fn main() {
     println!("Guess a number");
-
+   
     println!("Please input your guess: ");
 
-    let mut guess = String::new();
+    let mut guess: String = String::new();
     
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read the line");
+        .expect("Failed to load");
+        
 
     println!("You guessed: {guess}");
 }
