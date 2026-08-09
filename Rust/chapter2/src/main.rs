1533bb84075be78189c8306bd20253c384bdda6a
Aditya chauhan <satya.chauhan1976@gmail.com>
2026-08-09 23:28:09 +0530

    new version of rand - RngExt trait

--- Rust/chapter2/src/main.rs
+++ Rust/chapter2/src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::RngExt;
 
 fn main() {
     println!("Guess a number");
+
+    let secret_num: i32 = rand::rng().random_range(1..=100);
+
+    println!("Secret number is {}", secret_num);
    
     println!("Please input your guess: ");
 
